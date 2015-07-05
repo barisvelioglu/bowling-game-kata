d9148d31128209108eb603c11b3a5d0abcae8823
Uttam Kini <uttam@aplopio.com>
2015-07-05 19:11:52 +0530

    Fourth Test: Green 5
    Slide 50
    
    Refactor: Extract out the _is_strike_method for better readability

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game(object):
         score = 0
         frame_index = 0
         for frame in range(0, 10):
-            if self._rolls[frame_index] == 10:  # Strike
+            if self._is_strike(frame_index):
                 score += 10 + self._strike_bonus(frame_index)
                 frame_index += 1
             elif self._is_spare(frame_index):
@@ -32,3 +32,6 @@ class Game(object):
 
     def _is_spare(self, frame_index):
         return self._rolls[frame_index] + self._rolls[frame_index + 1] == 10
+
+    def _is_strike(self, frame_index):
+        return self._rolls[frame_index] == 10
