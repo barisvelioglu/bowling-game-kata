03700c9bfb7f4d9f720f92312c95b8792dfe0eb2
Uttam Kini <uttam@aplopio.com>
2015-07-05 18:05:16 +0530

    Third Test: Green 2
    Slide 34
    
    Refactor: Create private variables
    1. _current_roll to track the turn of the roll
    2. _rolls to keep track of each roll

--- game.py
+++ game.py
@@ -2,8 +2,12 @@ class Game(object):
     def __init__(self):
         super(Game, self).__init__()
         self._score = 0
+        self._rolls = [0 for i in range(21)]
+        self._current_roll = 0
 
     def roll(self, pins):
+        self._rolls[self._current_roll] = pins
+        self._current_roll += 1
         self._score += pins
 
     @property
