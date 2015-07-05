02c7420c358e0c5dbca490f1c95aa8cb858f6235
Uttam Kini <uttam@aplopio.com>
2015-07-05 19:05:46 +0530

    Fourth Test: Green 3
    Slide 48
    
    Fix the test_one_strike by putting in logic to handle the strike

--- game.py
+++ game.py
@@ -13,11 +13,16 @@ class Game(object):
         score = 0
         frame_index = 0
         for frame in range(0, 10):
-            if self._is_spare(frame_index):
+            if self._rolls[frame_index] == 10:  # Strike
+                score += 10 + self._rolls[frame_index + 1] + \
+                         self._rolls[frame_index + 2]
+                frame_index += 1
+            elif self._is_spare(frame_index):
                 score += 10 + self._rolls[frame_index + 2]
+                frame_index += 2
             else:
                 score += self._rolls[frame_index] + self._rolls[frame_index + 1]
-            frame_index += 2
+                frame_index += 2
         return score
 
     def _is_spare(self, frame_index):
