1023e995f063b7838526832b7ba945a86a87d44c
Uttam Kini <uttam@aplopio.com>
2015-07-05 17:55:09 +0530

    Second Test: Green 6
    Slide 28
    
    Refactor: Move the roll_many function to the top of the file

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -8,14 +8,14 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = Game()
 
+    def roll_many(self, pins, times):
+        for i in range(times):
+            self.game.roll(pins)
+
     def test_gutter_game(self):
         self.roll_many(pins=0, times=20)
 
         assert self.game.score == 0
-
-    def roll_many(self, pins, times):
-        for i in range(times):
-            self.game.roll(pins)
 
     def test_all_ones(self):
         self.roll_many(pins=1, times=20)
