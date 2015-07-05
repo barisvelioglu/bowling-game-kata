d650bd5462b0d72517e0c33a7a6559cb14747485
Uttam Kini <uttam@aplopio.com>
2015-07-05 17:32:14 +0530

    Second Test: Red 1
    Slide 22
    
    Test all ones, failing test when every roll has one pin falling.

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -12,3 +12,11 @@ class TestBowlingGame(unittest.TestCase):
 
         assert game.score == 0
 
+    def test_all_ones(self):
+        game = Game()
+
+        for i in range(20):
+            game.roll(1)
+
+        assert game.score == 20
+
